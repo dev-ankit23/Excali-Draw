0f38157926b8c9027661e8cef9631eaf7b2a3a2e
devankitx <ankitmaurya.coder@gmail.com>
2025-12-14 14:32:03 +0530

    changes in file

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello from ws-backend");
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    ws.send("pong");
+  });
+});
